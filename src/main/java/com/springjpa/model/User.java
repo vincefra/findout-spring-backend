798496ddda125e3b4ceeffdd22ae07fcbda15ef3
Vincent <vincent.frank@yh.nackademin.se>
2019-10-15 15:49:36 +0200

    updated userClass

--- src/main/java/com/springjpa/model/User.java
+++ src/main/java/com/springjpa/model/User.java
@@ -21,6 +21,8 @@ public class User {
 
     private String username;
     private String password;
+    private String firstName;
+    private String lastName;
     
     public User() {      
     }
@@ -53,5 +55,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
